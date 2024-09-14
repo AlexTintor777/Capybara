5e514e49da1f03d4092c49039f9276ef17a8b6f2
alex <alex@hot.com>
2024-09-14 16:21:51 -0300

    actualizado persona

--- src/persona.java
+++ src/persona.java
@@ -2,4 +2,16 @@ package src;
 
 public class persona {
     int balones = 0;
+
+    public persona(int balones) {
+        this.balones = balones;
+    }
+
+    public int getBalones() {
+        return balones;
+    }
+
+    public void setBalones(int balones) {
+        this.balones = balones;
+    }
 }
