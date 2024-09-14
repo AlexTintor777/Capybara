1264aeb4f0b43ccadaade8cccfc4be142bf5d4c4
alex <alex@hot.com>
2024-09-13 21:02:39 -0300

    Agregue persona

--- src/inicio.java
+++ src/inicio.java
@@ -1,5 +1,6 @@
 public static void main(String[] args) {
-    System.out.println("Hola mundo");
+    System.out.println("Hola mundois");
+    System.out.println("Hola mundois");
     System.out.println("Hola mundo soy alex");
     System.out.println("Hola mundo soy alex2");
 }
