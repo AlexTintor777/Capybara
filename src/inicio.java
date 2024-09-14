68b4b1e09464118e937786b7044fc40e148540c5
alex <alex@hot.com>
2024-09-13 23:25:40 -0300

    baloneos2

--- src/inicio.java
+++ src/inicio.java
@@ -3,4 +3,5 @@ public static void main(String[] args) {
     System.out.println("Hola mundois");
     System.out.println("Hola mundo soy alex");
     System.out.println("Hola mundo soy alex2");
+    System.out.println("Holasita");
 }
